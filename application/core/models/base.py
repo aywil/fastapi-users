7ef44027ba36d0e1a12213e2738c697e64d71e6b
aywil <imaybedeadz.gmail.com>
2025-12-13 22:33:39 +0700

    add namimg_convention for models

--- application/core/models/base.py
+++ application/core/models/base.py
@@ -1,3 +1,4 @@
+from sqlalchemy import MetaData
 from utils import camel_case_to_snake_case
 from sqlalchemy.orm import (
     DeclarativeBase,
@@ -5,10 +6,15 @@ from sqlalchemy.orm import (
     mapped_column,
     declared_attr,
 )
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(
+        naming_convention=settings.db.naming_convention,
+    )
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
