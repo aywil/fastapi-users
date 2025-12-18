5f34bf9b4f7cf7eedbbb14213165baedae2bd063
aywil <imaybedeadz@gmail.com>
2025-12-18 23:02:12 +0700

    create fastapi-users dependencies for users

--- application/api/api_v1/messages.py
+++ application/api/api_v1/messages.py
@@ -1,6 +1,16 @@
-from fastapi import APIRouter
-from core.config import settings
+from typing import Annotated
+from fastapi import (
+    APIRouter,
+    Depends,
+)
 
+from application.core.schemas.user import UserRead
+from core.models import User
+from core.config import settings
+from .fastapi_users import (
+    current_active_user,
+    current_active_superuser,
+)
 
 router = APIRouter(
     prefix=settings.api.v1.messages,
@@ -8,5 +18,35 @@ router = APIRouter(
 )
 
 
-def get_user_messages():
-    pass
+@router.get("")
+def get_user_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_user),
+    ],
+):
+    return {
+        "messsages": [
+            "m1",
+            "m2",
+            "m3",
+        ],
+        "user": UserRead.model_validate(user),
+    }
+
+
+@router.get("/secrets")
+def get_superuser_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+):
+    return {
+        "messsages": [
+            "secret-m1",
+            "secret-m2",
+            "secret-m3",
+        ],
+        "user": UserRead.model_validate(user),
+    }
